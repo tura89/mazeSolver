7be7685406b9e1bfbda932ea05573a4cf11e4941
tura89 <turdzeladze@gmail.com>
2023-06-04 22:51:43 +0400

    square, role, maze and border models

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from maze_solver.models.border import Border
+from maze_solver.models.maze import Maze
+from maze_solver.models.role import Role
+from maze_solver.models.square import Square
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+maze = Maze(
+     squares=(
+         Square(0, 0, 0, Border.TOP | Border.LEFT),
+         Square(1, 0, 1, Border.TOP | Border.RIGHT),
+         Square(2, 0, 2, Border.LEFT | Border.RIGHT, Role.EXIT),
+         Square(3, 0, 3, Border.TOP | Border.LEFT | Border.RIGHT),
+         Square(4, 1, 0, Border.BOTTOM | Border.LEFT | Border.RIGHT),
+         Square(5, 1, 1, Border.LEFT | Border.RIGHT),
+         Square(6, 1, 2, Border.BOTTOM | Border.LEFT),
+         Square(7, 1, 3, Border.RIGHT),
+         Square(8, 2, 0, Border.TOP | Border.LEFT, Role.ENTRANCE),
+         Square(9, 2, 1, Border.BOTTOM),
+         Square(10, 2, 2, Border.TOP | Border.BOTTOM),
+         Square(11, 2, 3, Border.BOTTOM | Border.RIGHT),
+     )
+ )
